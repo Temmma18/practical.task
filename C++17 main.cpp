410f90eadecdf061f018e932f3ce0162539421dd
Semenov Artemiy <artemon8116@gmail.com>
2026-03-14 14:36:21 +0300

    Create C++17 main.cpp

---
+++ C++17 main.cpp
@@ -0,0 +1,12 @@
+#include "config.hpp"
+#include <iostream>
+
+int main() {
+
+    std::cout << Config::kMax << "\n";
+
+    const int* p = &Config::kMax;
+    std::cout << *p << "\n";
+
+    return 0;
+}
